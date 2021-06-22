ef19db68693275206f844b68e248b3346be1608d
Suraj Khayamali <surajkhayamali.sk@gmail.com>
2021-06-22 16:13:41 +0545

    Using Methods to Add Behaviours to a Type

--- models/user.go
+++ models/user.go
@@ -13,6 +13,17 @@ var (
 	nextID = 1
 )
 
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+	return u, nil
+}
+
 func Demo() {
 	var u User
 	u.ID = 1
