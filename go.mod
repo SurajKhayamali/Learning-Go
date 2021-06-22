e25952e698922b868c0e33afeda384f4f2c2c9e7
Suraj Khayamali <surajkhayamali.sk@gmail.com>
2021-06-22 13:39:48 +0545

    Running tutorial codes from golang's website

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SurajKhayamali/learning-go
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/SurajKhayamali/learning-go/examples"
+)
+
+func main() {
+	examples.Hello()
+}
